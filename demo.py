a9343076479a29a4fc543b873dc7f757d68c6066
Jokcer <519548295@qq.com>
2024-11-16 21:04:04 +0800

    chore: fix demo

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import cv2
 
 from rapid_unwrap.inference import DocUnwrapper
 img_path = "img/demo.jpg"
-doc_unwrapper = DocUnwrapper(img_path)
+doc_unwrapper = DocUnwrapper()
 unwrapped_img, elapse = doc_unwrapper(img_path)
 print(f"doc unwrap elapse:{elapse}")
 cv2.imwrite("unwarped.png", unwrapped_img)
